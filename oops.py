1669fc8ec9dd37c9e518929a038ecd4c9d8cd082
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-07 18:19:59 +0530

    1st commit

---
+++ oops.py
@@ -0,0 +1,54 @@
+# Object orianted approach to handle errors in Python.
+# provide security to the code .
+# class Factory:
+#     a = 12 # /Attribute
+
+#     def hello(self): # /Method
+#         print("How are you")
+
+    # print("hello how are you i am getting initilized")
+
+# print(Factory().a)
+
+# Factory().hello()
+
+# obj = Factory() # /Object
+
+# print(obj.a)
+
+# obj.hello()
+
+# class Factory:
+#     a = 12 # /Attribute
+
+#     def hello(self): # /Method
+#         print("How are you")
+
+
+# obj = Factory()
+
+# obj2 = Factory()
+
+# obj.hello()
+
+# class Factory:
+#     def __init__(self,material,zips,pockets):
+#         self.material = material
+#         self.zips = zips
+#         self.pockets = pockets
+
+#     def show(self):
+#         print( f"your object details are: {self.material}, {self.zips} , {self.pockets}")
+
+# reebok = Factory("leather",3,2)
+
+# campus = Factory("nylon",3,3)
+
+
+# reebok.show()
+
+class Animal :
+    name = " lion " #class attribute 
+    def ___init__(self,age):
+        self.age = age # instance attribute
+
