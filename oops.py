8a709b4dff74ce3fef38d00389a6578320ec81c5
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-07 20:05:40 +0530

    11th  commit

--- oops.py
+++ oops.py
@@ -124,3 +124,19 @@
 
 # print(obj.name3)
 
+class Factory :
+    def __init__(self ,material,zips):
+        self.material = material
+        self.zips = zips
+
+class BhopalFactory(Factory):
+        def __init__(self,material,zips,color):
+             super().__init__(material,zips)
+             self.color = color
+
+class PuneFactory(Factory):
+        def __init__(self,material,zips,color,pockets):
+             super().__init__(material,zips,color)
+             self.pockets = pockets
+
+obj = PuneFactory()
